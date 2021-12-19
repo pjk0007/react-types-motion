840a14f124f13ba84b10dc76eb595082cc71e59e
pjk0007 <44570338+pjk0007@users.noreply.github.com>
2021-12-19 18:45:41 +0900

    learned useTransform

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useEffect } from "react";
 
 const Wrapper = styled.div`
@@ -19,13 +19,13 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   useEffect(() => {
-    x.onChange(() => console.log(x.get()));
+    scale.onChange(() => console.log(scale.get()));
   }, [x]);
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>click me</button>
-      <Box style={{ x }} drag dragSnapToOrigin />
+      <Box style={{ x, scale }} drag dragSnapToOrigin />
     </Wrapper>
   );
 }
