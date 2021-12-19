4955b0503d8be8895865581e74fb81f968ab98b3
pjk0007 <44570338+pjk0007@users.noreply.github.com>
2021-12-20 06:00:07 +0900

    learned layoutId to connect seperate components

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  flex-direction: column;
   background: linear-gradient(135deg, rgb(255, 159, 204), rgb(185, 63, 255));
 `;
 
@@ -18,8 +17,8 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -27,7 +26,6 @@ const Circle = styled(motion.div)`
   background-color: #00a5ff;
   height: 100px;
   width: 100px;
-  border-radius: 50%;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -36,14 +34,12 @@ function App() {
   const toggleClicked = () => setClicked((prev) => !prev);
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box
-        style={{
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start",
-        }}
-      >
-        <Circle layout />
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
       </Box>
+      <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
     </Wrapper>
   );
 }
